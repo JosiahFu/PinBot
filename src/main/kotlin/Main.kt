079ff4c1f70f4c52bf5b61c67497ce6e05953e5c
JosiahFu <josiahfu@gmail.com>
2024-09-07 01:40:18 -0700

    Error checks, better formatting, delete redundant messages

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,14 +2,27 @@
 
 package archives.tater.pinbot
 
+import dev.kord.common.entity.MessageType
+import dev.kord.common.entity.Snowflake
 import dev.kord.core.Kord
+import dev.kord.core.behavior.GuildBehavior
+import dev.kord.core.behavior.MessageBehavior
+import dev.kord.core.behavior.interaction.respondEphemeral
 import dev.kord.core.behavior.interaction.respondPublic
-import dev.kord.core.entity.effectiveName
 import dev.kord.core.event.gateway.ReadyEvent
-import dev.kord.core.event.interaction.MessageCommandInteractionCreateEvent
+import dev.kord.core.event.interaction.GlobalMessageCommandInteractionCreateEvent
+import dev.kord.core.event.interaction.GuildMessageCommandInteractionCreateEvent
+import dev.kord.core.event.message.MessageCreateEvent
 import dev.kord.core.on
 import io.github.cdimascio.dotenv.Dotenv
 import kotlinx.coroutines.launch
+
+fun linkTo(guildId: Snowflake, channelId: Snowflake, messageId: Snowflake) =
+    "https://discord.com/channels/$guildId/$channelId/$messageId"
+
+fun linkTo(guild: GuildBehavior, message: MessageBehavior) = linkTo(guild.id, message.channelId, message.id)
+
+suspend fun linkTo(message: MessageBehavior) = linkTo(message.asMessage().getGuild(), message)
 
 suspend fun main() {
     val dotenv = Dotenv.load()
@@ -17,21 +30,42 @@ suspend fun main() {
     with (Kord(dotenv["BOT_TOKEN"])) {
         createGlobalMessageCommand("Pin/Unpin")
 
-        on<MessageCommandInteractionCreateEvent> {
-            if (interaction.target.fetchMessage().isPinned) {
+        on<GuildMessageCommandInteractionCreateEvent> {
+            val message = interaction.target.fetchMessage()
+            if (message.type == MessageType.ChannelPinnedMessage || message.data.applicationId.value == resources.applicationId) {
+                interaction.respondEphemeral {
+                    content = "Cannot pin a pin message"
+                }
+                return@on
+            }
+
+            val name = interaction.user.effectiveName
+            if (message.isPinned) {
                 kord.launch {
                     interaction.respondPublic {
-                        content = "${interaction.user.effectiveName} unpinned a message"
+                        content = "**$name** unpinned ${linkTo(message)} from the channel."
                     }
                 }
                 interaction.target.unpin("Pin Bot")
             } else {
                 kord.launch {
                     interaction.respondPublic {
-                        content = "${interaction.user.effectiveName} pinned a message"
+                        content = "**$name** pinned ${linkTo(message)} to the channel."
                     }
                 }
                 interaction.target.pin("Pin Bot")
+            }
+        }
+
+        on<GlobalMessageCommandInteractionCreateEvent> {
+            interaction.respondPublic {
+                content = "Does not support global commands"
+            }
+        }
+
+        on<MessageCreateEvent> {
+            if (message.type == MessageType.ChannelPinnedMessage && message.data.author.id == selfId) {
+                message.delete("Redundant pin message")
             }
         }
 
